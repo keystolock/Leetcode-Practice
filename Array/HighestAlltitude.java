4aa458978114904678a6a3ef3aff1db3f17ccbc2
Abhishek Yadav <abhishekyadav142006@gmail.com>
2026-06-19 19:38:09 +0530

    Added Highest Alltitude Problem solution in java

---
+++ Array/HighestAlltitude.java
@@ -0,0 +1,14 @@
+public class HighestAlltitude {
+
+    public int largestAltitude(int[] gain) {
+        int alt = 0, mx = 0;
+
+        for (int x : gain) {
+            alt += x;
+            mx = Math.max(mx, alt);
+        }
+
+        return mx;
+    }
+}
+
