5c463ae150624e8e3964e2b2edb7a2302db64d34
Abhishek Yadav <abhishekyadav142006@gmail.com>
2026-07-18 18:10:53 +0530

    Added solution of Find Greatest Common Divisor of Array in java

---
+++ Maths/gcd.java
@@ -0,0 +1,14 @@
+class Solution {
+    int gcd(int a, int b) {
+        if(b == 0) return a; 
+        return gcd(b, a % b); 
+    }
+    public int findGCD(int[] nums) {
+        int mn = 1001, mx = 0; 
+        for(int i = 0; i < nums.length; i++) {
+            mn = Math.min(mn, nums[i]); 
+            mx = Math.max(mx, nums[i]); 
+        }
+        return gcd(mn, mx); 
+    }
+}
